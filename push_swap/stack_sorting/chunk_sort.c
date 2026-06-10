b275032864b2d71ec485d2579312b622b4ac10c8
AnitaMah <140920875+AnitaMah@users.noreply.github.com>
2026-06-10 17:35:27 +0200

    Fix chunk_sort push_chunks algorithm

--- push_swap/stack_sorting/chunk_sort.c
+++ push_swap/stack_sorting/chunk_sort.c
@@ -3,43 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   chunk_sort.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anmakhov <anmakhov@student.42.fr>          +#+  +:+       +#+        */
+/*   By: anita <anita@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/02 12:07:09 by anmakhov          #+#    #+#             */
-/*   Updated: 2026/06/10 17:32:44 by anmakhov         ###   ########.fr       */
+/*   Updated: 2026/06/03 08:39:33 by anita            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
-#include <stdio.h>
 
 void	push_chunks(t_stack *a, t_stack *b)
 {
 	int	max;
-	int	pushed;
+	int	size_at_start;
 
-	pushed = 0;
 	max = a->chunk_size;
 	while (a->size > 0)
 	{
-		fprintf(stderr, "DEBUG: a->top->index=%d, max=%d, pushed=%d,a->size=%d\n", a->top->index, max, pushed, a->size);
-		if (a->top->index <= max)
+		size_at_start = a->size;
+		while (a->size > 0)
 		{
-			pb(a, b);
-			pushed++;
-			fprintf(stderr, "  -> Pushed (pushed now=%d)\n", pushed);
+			if (a->top->index < max)
+				pb(a, b);
+			else
+				ra(a);
+			if (a->size == size_at_start)
+				break ;
 		}
-		else
-		{
-			ra(a);
-			fprintf(stderr, "  -> Rotated\n");
-		}
-		if (pushed == a->chunk_size)
-		{
-			fprintf(stderr, "  -> Chunk complete, moving to next chunk\n");
-			max += a->chunk_size;
-			pushed = 0;
-		}
+		max += a->chunk_size;
 	}
 }
 
@@ -48,8 +39,6 @@ void	init_chunks(t_stack *a)
 	a->chunk_size = a->size / 5;
 	if (a->chunk_size < 1)
 		a->chunk_size = 1;
-	fprintf(stderr, "DEBUG: init_chunks - size=%d, chunk_size=%d\n", a->size,
-		a->chunk_size);
 }
 
 void	chunk_sort(t_stack *a, t_stack *b)
